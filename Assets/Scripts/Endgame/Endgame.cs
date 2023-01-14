1f364b3512264b272ecac24a6b1741bcf790bee3
dao2506 <ledao2506@gmail.com>
2023-01-14 22:15:08 +0700

    Create Endgame.cs

---
+++ Assets/Scripts/Endgame/Endgame.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour
+{
+
+    public void loadCutScreen() {
+        UnityEngine.Application.LoadLevel("CutScene");
+    }
+
+    public void loadLv1() { 
+        UnityEngine.Application.LoadLevel("Level1");
+    }
+    public void loadSettingMenu(){
+        UnityEngine.Application.LoadLevel("SettingMenu");
+    }
+    public void loadChangePlayer(){
+        UnityEngine.Application.LoadLevel("ChoosePlayer");
+    }
+
+    public void loadCredit(){
+        UnityEngine.Application.LoadLevel("Credit");
+    }
+
+    public void loadScoreBoard() {
+        UnityEngine.Application.LoadLevel("ScoreBoard");
+    }
+
+    public void loadMainmenu()
+    {
+        UnityEngine.Application.LoadLevel("ScoreBoard");
+    }
+}
